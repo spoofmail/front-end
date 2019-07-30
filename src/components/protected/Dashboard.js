e1645784a58af3de03ed6d45379f2e449b59b647
Vincent Costa <costa.vincent132@gmail.com>
2019-07-30 11:36:36 -0700

    Added edit name onclick, added copy to clipboard onclick

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Button, Paper, InputBase, IconButton } from "@material-ui/core";
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Paper, InputBase, IconButton, TextField } from "@material-ui/core";
 import { Search, Cancel, KeyboardArrowDown, KeyboardArrowUp, Trash } from "@material-ui/icons"
 import { makeStyles } from '@material-ui/core/styles';
 
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt, faCopy } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /*import { Parser } from "html-to-react";
@@ -166,9 +166,45 @@ const style = { color: "var(--font-color)", fontSize: "2rem", cursor: "pointer" 
 const Address = ({ data, emails }) => {
     const [expanded, setExpanded] = useState(false);
 
+    const [nameEdit, setNameEdit] = useState(data.name);
+    const [editMode, setEditMode] = useState(false);
+
+    const [hoverMode, setHoverMode] = useState(false);
+
     const _renderChevron = _ => {
         if(!expanded) return <KeyboardArrowDown style = {style} onClick = {_ => setExpanded(true)}/>
         else return <KeyboardArrowUp style = {style} onClick = {_ => setExpanded(false)}/>
+    }
+
+    const _renderName = _ => {
+        if(editMode) return <form onSubmit = {handleEditBlur}><TextField value = {nameEdit} onChange = {handleNameEdit} onBlur = {handleEditBlur} /></form>;
+        else return <h3 onClick = {_ => setEditMode(true)}>{nameEdit}</h3>;
+    }
+
+    const handleEditBlur = _ => {
+        setEditMode(false);
+    }
+
+    const handleNameEdit = e => {
+        setNameEdit(e.target.value);
+    }
+
+    const _renderEmail = _ => {
+        if(hoverMode) return <h3 onMouseLeave = {handleMouseLeave} style = {{ cursor: "pointer", border: "1px solid var(--font-color)", padding: 5 }} onClick = {handleHoverClick}>{data.email} <FontAwesomeIcon icon = {faCopy} /></h3>;
+        else return <h3 onMouseOver = {handleMouseOver} >{data.email}</h3>;
+    }
+
+    const handleMouseOver = _ => {
+        setHoverMode(true);
+    }
+
+    const handleMouseLeave = _ => {
+        setHoverMode(false);
+    }
+
+    const handleHoverClick = _ => {
+        navigator.clipboard.writeText(data.email);
+        alert("Copied to clipboard");
     }
 
     return (
@@ -176,8 +212,8 @@ const Address = ({ data, emails }) => {
             <div className = "title">
                 <div>
                     { _renderChevron() }
-                    <h3>{data.name}</h3>
-                    <h3>{data.email}</h3>
+                    { _renderName() }
+                    { _renderEmail() }
                     <h3>{data.emails}</h3>
                 </div>
                 <Button variant = "outlined" style = {{ color: "red", borderColor: "red", textTransform: "none" }}>Remove</Button>

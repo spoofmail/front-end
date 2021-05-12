0c695eff59670da205a0abf05ea4294f1a9d5f01
VincentCosta6 <37250561+VincentCosta6@users.noreply.github.com>
2021-05-11 17:40:55 -0700

    chore: add websocket reconnect and pings

--- src/components/protected/dashboardComponents/WebsocketHandler.js
+++ src/components/protected/dashboardComponents/WebsocketHandler.js
@@ -1,13 +1,14 @@
-import React, { useCallback, useContext, useEffect, useRef } from 'react'
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
 
 import EmailContext from '../../../stores/email-store'
-import Websocket from '../../../stores/socket-store'
 
 import Cookies from "universal-cookie";
 let cookies = new Cookies();
 
 export default (props) => {
     const context = useContext(EmailContext)
+
+    const [interval, setIntervalState] = useState(null)
 
     const websocket = useRef(null)
 
@@ -17,13 +18,40 @@ export default (props) => {
             props.addEmail(message)
     }, [context.emailMap, props.addEmail])
 
-    useEffect(() => {
+    const connectWebsocket = () => {
         websocket.current = new WebSocket(`wss://spoofmail-lambda.herokuapp.com/ws?token=${cookies.get("token")}`)
-        websocket.current.onopen = () => context.setWebsocketOpen(true)
-        websocket.current.onclose = () => console.log("ws closed")
+        websocket.current.onopen = () => {
+            context.setWebsocketOpen(true)
+            setIntervalState(setInterval(() => {
+                console.log(websocket.current)
+                if (websocket.current.readyState === WebSocket.OPEN) {
+                    websocket.current.send(JSON.stringify({ msg: 'ping' }))
+                    console.log('ping')
+                }
+            }, 15000))
+        }
 
+        websocket.current.onerror = () => {
+            connectWebsocket()
+        }
+
+        websocket.current.onclose = () => {
+            connectWebsocket()
+            console.log('closed')
+            context.setWebsocketOpen(false)
+        }
+    }
+
+    useEffect(() => {
+        connectWebsocket()
         return () => {
-            websocket.current.close()
+            if (websocket.current && websocket.current.readyState === WebSocket.OPEN) {
+                websocket.current.close()
+                websocket = null
+            }
+            if (interval) {
+                clearInterval(interval)
+            }
         };
     }, [])
 
